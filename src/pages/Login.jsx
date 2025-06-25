33500928c9729d9688bbd312f4461c7fa26490db
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-06-25 23:01:33 +0200

    first front commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLoginUser(event){
+    event.preventDefault();
+    const userToLogin = {email, password};
+    axios.post("http://localhost:5005/auth/login", userToLogin)
+    .then((res)=>{
+      console.log("user successfully logged in", res.data);
+      // I have to figure out for user profile is it doctor or patient
+    })
+    .catch((err) => console.log(err))
+  }
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleLoginUser}>
+        <label>email
+          <input type='email' value={email} onChange={(event)=>setEmail(event.target.value)}/>
+        </label>
+        <label>password
+          <input type='password' value={password} onChange={(event)=>setPassword(event.target.value)}/>
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Signup() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [description, setDescription] = useState('')
+  const [role, setRole] = useState('')
+  const nav = useNavigate();
+
+  function handleSignupUser(event) {
+    event.preventDefault()
+    const userToCreate = { firstName, lastName, email, password, description, role }
+    axios.post('http://localhost:5005/auth/signup', userToCreate)
+      .then((res) => {
+        console.log('user successfully created', res.data)
+        nav("/login")
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <form onSubmit={handleSignupUser}>
+        <label>
+          Firstname:
+          <input type='text' value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+        </label>
+        <label>
+          Lastname:
+          <input type='text' value={lastName} onChange={(event) => setLastName(event.target.value)} />
+        </label>
+        <label>
+          Email:
+          <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} />
+        </label>
+        <label>
+          Description:
+          <textarea value={description} onChange={(event) => setDescription(event.target.value)} />
+        </label>
+        <label>
+          Your role:
+          <input type='text' value={role} onChange={(event) => setRole(event.target.value)} />
+        </label>
+        <button>Sign up</button>
+      </form>
+    </div>
   )
 }
 
