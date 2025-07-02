7d9c2b21adeb1a05a9d75ff800817809f3b25b20
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-07-02 17:17:12 +0200

    profile

--- src/pages/CreateDoctorProfile.jsx
+++ src/pages/CreateDoctorProfile.jsx
@@ -33,8 +33,57 @@ async function doctorInfoHandle(event) {
   return (
     <>
      <form onSubmit={doctorInfoHandle}>
-      <label>specialty
-        <input type="text" onChange={(event)=>{setSpecialty(event.target.value)}} />
+      <label>Specialty
+        <select
+        id="specialty"
+        value={specialty}
+        onChange={(event) =>setSpecialty(event.target.value)}>
+        <option value="">-- Select a specialty --</option>
+
+        {/* Internal Medicine */}
+        <optgroup label="Internal Medicine & Subspecialties">
+          <option value="Internal Medicine">Internal Medicine</option>
+          <option value="Cardiology">Cardiology</option>
+          <option value="Endocrinology">Endocrinology</option>
+          <option value="Gastroenterology">Gastroenterology</option>
+          <option value="Hematology">Hematology</option>
+          <option value="Infectious Disease">Infectious Disease</option>
+          <option value="Nephrology">Nephrology</option>
+          <option value="Oncology">Oncology</option>
+          <option value="Pulmonology">Pulmonology</option>
+          <option value="Rheumatology">Rheumatology</option>
+          <option value="Geriatrics">Geriatrics</option>
+          <option value="Allergy and Immunology">Allergy and Immunology</option>
+        </optgroup>
+
+  
+
+        {/* Neurology & Psychiatry */}
+        <optgroup label="Neurology & Psychiatry">
+          <option value="Neurology">Neurology</option>
+          <option value="Clinical Neurophysiology">Clinical Neurophysiology</option>
+          <option value="Pain Medicine">Pain Medicine</option>
+          <option value="Psychiatry">Psychiatry</option>
+          <option value="Child and Adolescent Psychiatry">Child and Adolescent Psychiatry</option>
+          <option value="Forensic Psychiatry">Forensic Psychiatry</option>
+          <option value="Geriatric Psychiatry">Geriatric Psychiatry</option>
+          <option value="Addiction Psychiatry">Addiction Psychiatry</option>
+        </optgroup>
+
+
+        {/* Organ or System Specialties */}
+        <optgroup label="Organ/System Specialties">
+          <option value="Dermatology">Dermatology</option>
+          <option value="Ophthalmology">Ophthalmology</option>
+          <option value="Otolaryngology">Otolaryngology (ENT)</option>
+          <option value="Urology">Urology</option>
+          <option value="Gynecology and Obstetrics">Gynecology and Obstetrics</option>
+          <option value="Anesthesiology">Anesthesiology</option>
+          <option value="Radiology">Radiology</option>
+          <option value="Interventional Radiology">Interventional Radiology</option>
+          <option value="Pathology">Pathology</option>
+        </optgroup>
+      </select>
       </label>
        <label>address
         <input type="text" onChange={(event)=>{setAddress(event.target.value)}} />
