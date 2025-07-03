1eb91c3ab3b4a89e3f67db07fd6a5caf2a06212b
Maedeh98a <maedeh.ahmadian98@gmail.com>
2025-07-04 00:36:13 +0200

    front changes

--- src/pages/CreatePatientProfile.jsx
+++ src/pages/CreatePatientProfile.jsx
@@ -4,6 +4,27 @@ import {config} from "../../config.js";
 import { useNavigate, useParams } from 'react-router-dom';
 
 function CreatePatientProfile() {
+    const diseaseOptions = [
+                  "Cancer",
+                  "Diabetes",
+                  "Hypertension (High Blood Pressure)",
+                  "Heart Disease",
+                  "Asthma",
+                  "Chronic Obstructive Pulmonary Disease (COPD)",
+                  "Stroke",
+                  "Alzheimer’s Disease",
+                  "Parkinson’s Disease",
+                  "Depression",
+                  "Anxiety Disorder",
+                  "Arthritis",
+                  "Obesity",
+                  "Chronic Kidney Disease",
+                  "Liver Disease",
+                  "HIV/AIDS",
+                  "COVID-19",
+                  "Influenza (Flu)",
+                  "Tuberculosis",
+                ];
 
     const [dateOfBirth, setDateOfBirth] = useState("1900-01-01");
     const[history, setHistory] = useState([]);
@@ -27,13 +48,18 @@ function CreatePatientProfile() {
 }
   return (
     <>
-     <form onSubmit={patientInfoHandle}>
+     <form onSubmit={patientInfoHandle} className='create-patient-form'>
       <label>dateOfBirth
         <input type="date" onChange={(event)=>{setDateOfBirth(event.target.value)}} />
       </label>
-      <label> history
-        <input type="text" onChange={(event)=>{setHistory(event.target.value)}}/>
-      </label>
+     <select value={history} onChange={(event)=>{setHistory(event.target.value)}}>
+      <option value="">--Please choose one--</option>
+      {diseaseOptions.map((disease, index) =>(
+        <option key={index} value={disease}>
+          {disease}
+        </option>
+      ))}
+    </select>
       
       <button>submit</button>
     </form>
